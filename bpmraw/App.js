d019e7ef03b923df765fa20c5b0d8befa68013f8
atiftariq786 <atiftariq786@hotmail.com>
2023-04-09 23:10:39 -0700

    what I changed

--- bpmraw/App.js
+++ bpmraw/App.js
@@ -1,60 +1,37 @@
-/* eslint-disable no-unused-vars */
-import {useCallback, useState, useEffect} from 'react';
-//import { StatusBar } from "expo-status-bar";
-import {StyleSheet, Text, View} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import StartGameScreen from './src/screens/StartGameScreen';
-import MainMenuScreen from './src/screens/MainMenuScreen';
-import MissionListScreen from './src/screens/MissionListScreen';
-import SuperHeroesProfile from './src/screens/SuperHeroesProfile';
-import SaveScore from './src/screens/SaveScore';
-import SettingScreen from './src/screens/SettingScreen';
-import {Provider} from 'react-redux';
-import store from './store/redux/Store';
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
 
-//import * as SplashScreen from "expo-splash-screen";
-
-const Stack = createNativeStackNavigator();
-//SplashScreen.preventAutoHideAsync();
-
-export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
-
-  setTimeout(() => setAppIsReady(true), 3000);
-
-  if (appIsReady) {
-    SplashScreen.hideAsync();
-  }
+const App = () => {
   return (
-    <>
-      <Provider store={store}>
-        {/* <StatusBar style="light" /> */}
-        <NavigationContainer>
-          <Stack.Navigator
-            initialRouteName={'Main'}
-            screenOptions={{
-              headerShown: false,
-            }}>
-            <Stack.Screen
-              style={styles.rootContainer}
-              name="startScreen"
-              component={StartGameScreen}
-            />
-            <Stack.Screen name="Main" component={MainMenuScreen} />
-            <Stack.Screen name="Save" component={SaveScore} />
-            <Stack.Screen name="Missions" component={MissionListScreen} />
-            <Stack.Screen name="SuperHeros" component={SuperHeroesProfile} />
-            <Stack.Screen name="Setting" component={SettingScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </Provider>
-    </>
+    <View style={styles.mainView}>
+      <View style={styles.basicview}>
+        <Text style={styles.basicText}>Text for view 11</Text>
+      </View>
+      <View style={styles.basicview}>
+        <Text style={styles.basicText}>Text for view 22</Text>
+      </View>
+    </View>
   );
-}
-//====================
+};
+
 const styles = StyleSheet.create({
-  rootContainer: {
+  mainView: {
     flex: 1,
+    paddingTop: 50,
+    alignItems: 'flex-start', // center x axis
+    justifyContent: 'flex-start', // center y axis
+  },
+  basicview: {
+    backgroundColor: 'green',
+    width: '100%',
+    marginBottom: 5,
+  },
+  basicText: {
+    fontSize: 20,
+    color: '#ffffff',
+    textAlign: 'center',
+    padding: 20,
   },
 });
+
+export default App;
